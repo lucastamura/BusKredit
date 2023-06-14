2ff5eba88981228f343fc82af8e177708ce35aed
lucastamura <lucas10.tamura@gmail.com>
2023-06-13 23:10:30 -0300

    add: files buskredit

---
+++ script.js
@@ -0,0 +1,16 @@
+function atualizaValores(){
+    var select = document.querySelector('#origem');
+    var optionValue = select.options[select.selectedIntex].value;
+
+    var value = optionValue.value;
+    var text = optionValue.text;
+
+    console.log(optionValue, text);
+}
+
+atualizaValores()
+
+if (mato>pessoa){
+    function capinar();
+}
+
